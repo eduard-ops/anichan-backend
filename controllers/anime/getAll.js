4d7615f05d6ccbb6d4a6eb434d51fd44e4586cca
eduard-ops <epustynnik@gmail.com>
2022-11-27 23:11:45 +0200

    add

---
+++ controllers/anime/getAll.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+const getAll = (req, res, next) => {
+  // axios
+  //   .get("https://kitsu.io/api/edge/anime")
+  // eslint-disable-next-line no-undef
+  // const response = await axios.get("https://kitsu.io/api/edge/anime");
+  // eslint-disable-next-line no-undef
+  // fetch("https://kitsu.io/api/edge/anime").then((c) => console.log(c));
+};
+
+module.exports = getAll;
---
+++ routes/api/anime.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { anime } = require("../../controllers");
+
+const ctrlWrapper = require("../../helpers/ctrlWrapper");
+
+router.get("/", ctrlWrapper(anime.getAll));
+
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
